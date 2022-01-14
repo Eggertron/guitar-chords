4292085086b6823b374c83bca0e27f79a8b98580
Egger Haaaaan <bboyhan@hotmail.com>
2022-01-14 18:39:00 -0500

    Create Love and Memories - O.A.R

---
+++ Love and Memories - O.A.R
@@ -0,0 +1,116 @@
+O.A.R
+Love and Memories
+ 
+Capo 2nd
+ 
+Chords:
+D: xx0232
+Dmaj7: x4x232
+A: x02220
+G: 320033
+Bm: x24432
+Csus2: x35533
+Bbsus2:x13311
+ 
+Intro:
+ 
+A Bm G D (4x)
+ 
+Verse 1:
+ 
+Dmaj7  D
+Love - ly, your always
+Dmaj7  A
+  Lovely
+  G                        Dmaj7 D
+A vision, you were the one.
+Dmaj7 D                         A
+Now...I...am stuck inside a memory
+                     Bm
+You forgot about our destiny.
+               G
+You buried me, didn't you?
+ 
+Chorus 1:
+           A    Bm  G              D
+Didn't you love me faster than the devil?
+A   Bm   G               D
+Run me straight into the ground
+A    Bm    G              D
+Drownin' deep inside your water
+A    Bm    G              D
+Drownin' deep inside your sound
+ 
+Verse 2:
+ 
+Dmaj7 D
+        You're always
+Dmaj7 A
+  Floating
+    G                      Dmaj7 D
+A vapor that I couldn't see
+Dmaj7  D                     A
+Here...I...am stuck inside a yesterday
+             Bm
+Everything has given way
+                 G
+You fell for me, didn't you?
+ 
+Chorus 2:
+           A    Bm  G              D
+Didn't you love me faster than the devil?
+A   Bm   G               D
+Run me straight into the ground
+A    Bm    G              D
+Drownin' deep inside your water
+A    Bm    G              D
+Drownin' deep inside your sound
+A    Bm  G              D
+Love me faster than the devil?
+A   Bm   G               D
+Run me straight into the ground
+A    Bm    G              D
+Drownin' deep inside your water
+A     Bm  G           D
+Drown in love and memories
+ 
+Interlude:
+ 
+D Csus2 Bbsus2 A
+ 
+Bridge:
+ 
+Dmaj7 D
+Maybe I am a crowded mind
+              Dmaj7 A
+I watch your eyes...glaze over, stare down at the floor
+         Dmaj7      G
+You were amazing to me, I was amazing to you
+    Dmaj7  D
+But here...we go...again
+ 
+Chours 3/Ending:
+           A    Bm  G              D
+Didn't you love me faster than the devil?
+A   Bm   G               D
+Run me straight into the ground
+A    Bm    G              D
+Drownin' deep inside your water
+A    Bm    G              D
+Drownin' deep inside your sound
+A    Bm  G              D
+Love me faster than the devil?
+A   Bm   G               D
+Run me straight into the ground
+A    Bm    G              D
+Drownin' deep inside your water
+A    Bm    G              D
+Drownin' deep inside your sound
+A    Bm  G              D
+Love me faster than the devil?
+A   Bm   G               D
+Run me straight into the ground
+A    Bm    G              D
+Drownin' deep inside your water
+A     Bm  G           D
+Drown in love and memories
